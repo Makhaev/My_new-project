9f3b3adf12b7efcb943bacefb23368894fabacf6
Adam <maxaev.adam@bk.ru>
2025-09-30 11:50:43 +0300

    new_commit_

--- db/db.go
+++ db/db.go
@@ -16,7 +16,7 @@ var DB *gorm.DB
 
 func Init() {
 	var err error
-	DB, err = gorm.Open(sqlite.Open("/root/My_new-project/gopher.db"), &gorm.Config{})
+	DB, err = gorm.Open(sqlite.Open("gopher.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatal("failed to connect database")
 	}
