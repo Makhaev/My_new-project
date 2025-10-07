bff5eb823ec8e73d4f5ad146339234368a48b8c5
Adam <maxaev.adam@bk.ru>
2025-10-07 14:16:35 +0300

    restartCommit

--- handlers/sendSMS.go
+++ handlers/sendSMS.go
@@ -29,7 +29,7 @@ func SendSMS(w http.ResponseWriter, r *http.Request) {
 	code := generathion.GenarathionCode(6)
 	expiresAt := time.Now().Add(5 * time.Minute)
 
-	query := `INSERT INTO sms_codes(phone,code,expires_at) VALUES(?,?,?)`
+	query := `INSERT INTO sms_codes(phone,code) VALUES(?,?)`
 
 	_, err := db.DB.Exec(query, req.Phone, code, expiresAt)
 	if err != nil {
@@ -44,8 +44,6 @@ func SendSMS(w http.ResponseWriter, r *http.Request) {
 		return
 
 	}
-
-	//
 
 	req.Text = fmt.Sprintln("Ваш код авторизации ", code)
 	fmt.Println("код отправлен ", req.Text)
