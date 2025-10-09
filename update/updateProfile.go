6ea21811dd9f537c79e4082779a5a9aa3428485c
Adam <maxaev.adam@bk.ru>
2025-10-09 09:49:02 +0300

    refactoringUpdateProfile

--- update/updateProfile.go
+++ update/updateProfile.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"main.go/db"
 	"main.go/middalware"
@@ -19,54 +20,70 @@ func UpdateProfile(w http.ResponseWriter, r *http.Request) {
 	}
 	userPhone := userIDRaw.(string)
 
-	// Парсим multipart/form-data
-	err := r.ParseMultipartForm(10 << 20) // 10MB
-	if err != nil {
-		http.Error(w, "Ошибка парсинга формы", http.StatusBadRequest)
-		return
-	}
+	var firstName, lastName, storeAddress, storeImagePath string
 
-	firstName := r.FormValue("first_name")
-	lastName := r.FormValue("last_name")
-	storeAddress := r.FormValue("store_address")
+	contentType := r.Header.Get("Content-Type")
 
-	// Получаем файл
-	file, handler, err := r.FormFile("storeImage")
-	var storeImagePath string
-	if err == nil {
-		defer file.Close()
-
-		dir := "uploads"
-		os.MkdirAll(dir, os.ModePerm)
-
-		storeImagePath = filepath.Join(dir, handler.Filename)
-		out, err := os.Create(storeImagePath)
-		if err != nil {
-			http.Error(w, "Ошибка сохранения изображения", http.StatusInternalServerError)
+	// 🧩 1. Если JSON
+	if strings.HasPrefix(contentType, "application/json") {
+		var data map[string]string
+		if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+			http.Error(w, "Ошибка парсинга JSON", http.StatusBadRequest)
 			return
 		}
-		defer out.Close()
+		firstName = data["first_name"]
+		lastName = data["last_name"]
+		storeAddress = data["store_address"]
+	}
 
-		_, err = out.ReadFrom(file)
-		if err != nil {
-			http.Error(w, "Ошибка записи изображения", http.StatusInternalServerError)
+	// 🧩 2. Если multipart/form-data (форма + изображение)
+	if strings.HasPrefix(contentType, "multipart/form-data") {
+		if err := r.ParseMultipartForm(10 << 20); err != nil {
+			http.Error(w, "Ошибка парсинга формы", http.StatusBadRequest)
 			return
+		}
+
+		firstName = r.FormValue("first_name")
+		lastName = r.FormValue("last_name")
+		storeAddress = r.FormValue("store_address")
+
+		file, handler, err := r.FormFile("storeImage")
+		if err == nil {
+			defer file.Close()
+
+			dir := "uploads"
+			os.MkdirAll(dir, os.ModePerm)
+
+			storeImagePath = filepath.Join(dir, handler.Filename)
+			out, err := os.Create(storeImagePath)
+			if err != nil {
+				http.Error(w, "Ошибка сохранения изображения", http.StatusInternalServerError)
+				return
+			}
+			defer out.Close()
+
+			_, err = out.ReadFrom(file)
+			if err != nil {
+				http.Error(w, "Ошибка записи изображения", http.StatusInternalServerError)
+				return
+			}
 		}
 	}
 
-	// Обновляем в базе
+	// 🧩 3. Обновляем в базе
 	query := `
 	UPDATE users
 	SET first_name = ?, last_name = ?, store_address = ?, store_image = ?
 	WHERE phone = ?
 	`
 
-	_, err = db.DB.Exec(query, firstName, lastName, storeAddress, storeImagePath, userPhone)
+	_, err := db.DB.Exec(query, firstName, lastName, storeAddress, storeImagePath, userPhone)
 	if err != nil {
 		http.Error(w, "Ошибка обновления пользователя", http.StatusInternalServerError)
 		return
 	}
 
+	// 🧩 4. Ответ
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{
 		"status":  "success",
